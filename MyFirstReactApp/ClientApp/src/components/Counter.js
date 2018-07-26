4af95e953422b7d0f9c8e4f32fb670c1cc71b1c4
kicrujdotcz <andrew.jurcik+github@gmail.com>
2018-07-26 12:33:50 +0200

    Decrement button to counter added

--- MyFirstReactApp/ClientApp/src/components/Counter.js
+++ MyFirstReactApp/ClientApp/src/components/Counter.js
@@ -7,11 +7,17 @@ export class Counter extends Component {
     super(props);
     this.state = { currentCount: 0 };
     this.incrementCounter = this.incrementCounter.bind(this);
+    this.decrementCounter = this.decrementCounter.bind(this);
   }
 
   incrementCounter() {
     this.setState({
       currentCount: this.state.currentCount + 1
+    });
+  }
+  decrementCounter() {
+    this.setState({
+      currentCount: this.state.currentCount - 1
     });
   }
 
@@ -25,6 +31,7 @@ export class Counter extends Component {
         <p>Current count: <strong>{this.state.currentCount}</strong></p>
 
         <button onClick={this.incrementCounter}>Increment</button>
+        <button onClick={this.decrementCounter}>Decrement</button>
       </div>
     );
   }
